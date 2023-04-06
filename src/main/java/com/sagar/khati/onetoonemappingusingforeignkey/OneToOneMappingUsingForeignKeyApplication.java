9e6937add7e3ee6e4e62f3d79447d3f20bd1a4b3
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-06 23:36:31 +0530

    Initial commit

---
+++ src/main/java/com/sagar/khati/onetoonemappingusingforeignkey/OneToOneMappingUsingForeignKeyApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.khati.onetoonemappingusingforeignkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneMappingUsingForeignKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneMappingUsingForeignKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/khati/onetoonemappingusingforeignkey/OneToOneMappingUsingForeignKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.khati.onetoonemappingusingforeignkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneMappingUsingForeignKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
